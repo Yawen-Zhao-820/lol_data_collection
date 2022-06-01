f20ca87f23347e8d577fcce66edb9adb2c3ed255
Yawen Zhao <77918358+Yawen-Zhao-820@users.noreply.github.com>
2022-06-02 00:09:24 +1000

    Add files via upload

--- data_log.py
+++ data_log.py
@@ -1,6 +1,8 @@
 from cProfile import run
 import pyautogui, sys
 import time
+import os
+from playsound import playsound
 # import mouse
 # import keyboard
 # import win32api
@@ -8,7 +10,7 @@ import threading
 from pynput import mouse, keyboard
 # import asyncio
 
-# while True:
+# while True:qwer
 #     mouse_events = []
 #     mouse.hook(mouse_events.append)
 #     keyboard.start_recording()
@@ -63,18 +65,18 @@ from pynput import mouse, keyboard
 
     
 
-#take screenshot
+# take screenshot'
 
-i = 0
+# i = 0
 
 
-while True:
-    time.sleep(1)
-    timestamp = time.time() 
-    filename = str(timestamp) + ".png"
-    src1 = pyautogui.screenshot(region=(768, 336, 1024, 768))
-    src1.save(filename)
-    i += 1
+# while True:
+#     time.sleep(1)
+#     timestamp = time.time() 
+#     filename = str(timestamp) + ".png"
+#     src1 = pyautogui.screenshot()
+#     src1.save(filename)
+#     i += 1
 
 # get mouse state
 # while True:
@@ -94,61 +96,109 @@ while True:
 
 
 # 事件驱动
-# 是否自动施法? 非自动施法就会有左键点击的操作
+# 是否自动施法? 自动施法就会有左键点击的操作
 # 不使用A键和S键盘，只用规定的键
 
 # Get screenshot while mouse clicked
 # Record Timestamp, mouse_locaton, mouse_isclicked_left, mouse_isclicked_right
 # Save screenshot with name as timestamp.png 
 
+
+
 # Get screenshot while keyboard pressed
-# Record Timestamp, Q, W, E, R
+# Record Timestamp, A, S, Q, W, E, R
 # Save screenshot with name as timestamp.png
 
 
-# VALID_KEYBOARD_INPUTS = ['q', 'w', 'e', 'r']
-# VALID_MOUSE_INPUTS = ['LD', 'LU', 'RD', 'RU']
+VALID_INPUTS = ['a', 's', 'q', 'w', 'e', 'r']
 
-# keyboard_input_sequence = []
-# mouse_input_sequence = []
 
-# def on_press(key):
-#     if key == keyboard.Key.esc:
-#         return False
-#     try:
-#         k = key.char
-#     except:
-#         k = key.name
-#     if k in VALID_KEYBOARD_INPUTS:
-#         keyboard_input_sequence.append(k)
-#         print(k + ' key is pressed at ' + str(time.time()) + '.')
+class Screenshotter:
+    def __init__(self):
+        self.dirname = os.path.dirname(__file__)
+        self.start_time = time.time()
+        self.keyboard_listener = keyboard.Listener(on_press=self.on_press)
+        self.mouse_listener = mouse.Listener(on_click=self.on_click)
+        self.mouse_controller = mouse.Controller()
+        self.start_listener()
+        # with self.keyboard_listener, self.mouse_listener:
+        #     self.keyboard_listener.join()
+        #     self.mouse_listener.join()
 
-# def on_click(x, y, button, pressed):
-#     if button == mouse.Button.left:
-#         if pressed:
-#             m = 'LD'
-#         else:
-#             m = 'LU'
-#         mouse_input_sequence.append(m)
+    def on_press(self, key):
+        if key == keyboard.Key.f8:
+            print('Monitoring is stopped')
+            # return False
+            self.stop_listener()
+        try:
+            k = key.char
+            pos = self.mouse_controller.position
+        except:
+            k = key
+            pos = self.mouse_controller.position
+        if k in VALID_INPUTS:
+            event = k + '_' + str(pos)
+            self.take_screenshot(event)
+            print(k + ' pressed at ' + str(pos) + ', time: ' + str(self.timestamp()) + '.')
 
-#         print('{0} at {1}'.format(
-#             'Left Pressed' if pressed else 'Left Released',
-#             (x, y)))
-#     if button == mouse.Button.right:
-#         if pressed:
-#             m = 'RD'
-#         else:
-#             m = 'RU'
-#         mouse_input_sequence.append(m)
+    def on_click(self, x, y, button, pressed):
+        if button == mouse.Button.middle:
+            print('Mouse monitoring is stopped ')
+            return False
+        if pressed:
+            event = f'mr_({x},{y})' if button == mouse.Button.right else f'ml_({x},{y})'
+            self.take_screenshot(event)
+            print('{0} at {1}, time:{2}'.format(
+                'Right clicked' if button == mouse.Button.right else 'Left clicked',
+                # 'Right clicked',
+                (x, y), self.timestamp()))
 
-#         print('{0} at {1}'.format(
-#             'Right Pressed' if pressed else 'Right Released',
-#             (x, y)))
+    def start_listener(self):
+        with self.keyboard_listener, self.mouse_listener:
+            filename = 'recording_start.wav'
+            path = os.path.join(self.dirname, r'Recording', filename)
+            playsound(path)
+            self.keyboard_listener.join()
+            self.mouse_listener.join()
+            
 
-# with (
-#     keyboard.Listener(on_press=on_press) as keyboard_listener, 
-#     mouse.Listener(on_click=on_click) as mouse_listener
-#     ):
-#     keyboard_listener.join()
-#     mouse_listener.join()
+    def stop_listener(self):
+        self.keyboard_listener.stop()
+        self.mouse_listener.stop()
+        filename = 'recording_end.wav'
+        path = os.path.join(self.dirname, r'Recording', filename)
+        playsound(path)
+        
+    def timestamp(self):
+        return time.time() - self.start_time
+
+    def take_screenshot(self, event):
+        filename = str(self.timestamp()) + '_' +str(event) + ".jpg"
+        path = os.path.join(self.dirname, r'Screenshots', filename)
+        try:
+            img = pyautogui.screenshot()
+            img.save(path)
+        except Exception as e:
+            print(e+ '\n' + 'At path: ' + path)
+
+
+def main():
+    def on_f7_press(key):
+        if keyboard.Key.f7 == key:
+            screenshotter = Screenshotter()
+
+        if keyboard.Key.f8 == key:
+            playsound(os.getcwd() + '\\' + r'Recording\recording_end.wav')
+            listener.stop()
+
+
+    # Collect events until released
+    with keyboard.Listener(
+            on_press=on_f7_press) as listener:
+        listener.join()
+    # screenshotter = Screenshotter()
+
+if __name__ == '__main__':
+    main()
+
 
